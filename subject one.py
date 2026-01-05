9b38a8ed0c232be9f1ec25ceb67efc0b2ebae76b
ammarkhafaja <ammarkhafaja92@gmail.com>
2026-01-05 21:20:57 +0300

    subject one finished subject two finished

--- subject one.py
+++ subject one.py
@@ -1,50 +1,49 @@
-age, monthly_income, loan_value, monthly_payment, insurance, total_monthly_payment = 0, 0, 0, 0, 0, 0
-
+age, monthly_income, loan_value, monthly_payment, insurance, total_monthly_payment,ban_reason = 0, 0, 0, 0, 0, 0,[]
+def reinsert(note):#تعمل هذه الدالة على تكرار عملية الادخال حتى يقوم المستخدم بادخال قيمة عددية موجبة
+    value = input(note)
+    while True:
+        if value.isdigit():
+            if int(value) > 1:
+                return int(value)
+        value = input(note)
 def check_numeric_value(value,note):
     #تعمل هذه الدالة على التأكد من أن القيمة المدخلة رقمية وموجبة
-    if value.isdigit(): #يتم التحقق من أن القيمة المدخلة رقمية
-        if int(value) >0:# يتم التحقق من أن الرقم المدخل موجب
+    if value.isdigit() : #يتم التحقق من أن القيمة المدخلة رقمية
+        if int(value) >1:# يتم التحقق من أن الرقم المدخل موجب في حال عدم تحقق أحد الشرطين يتم استدعاء الدالة Resinsert
             return int(value)
+        else:reinsert(note)
     else:
-
-        value = input(note)
-        while True:
-            if value.isdigit():
-                if int(value) > 0:
-                    return int(value)
-            value = input(note)
-
+        return reinsert(note)
+# القائمة  ban_reason سوف تحوي الأسباب الأولية التي سوف تمنع المصرف من اعطاء القرض وهي العمر - الدخل الشهري - قيمة القرض
 def getting_loan():
     age = input("يرجى ادخال العمر: ")
-    age = check_numeric_value(age,"يرجى ادخال العمر بشكل صحيح")
-    if age >=21 and age <65:
-        # يتم التحقق من أن العمر المدخل  يحقق الشرط المطلوب قبل ادخال باقي معلومات المستخدم
-        # لأنه لا حاجة لجعل المستخدم يدخل باقي المعلومات كون القرض سوف يرفض بسبب شرط العمر
-        monthly_income=input("ادخل قيمة الدخل الشهري: ")
-        monthly_income=check_numeric_value(monthly_income,"يرجى ادخال قيمة الدخل الشهري بشكل صحيح")
-        if monthly_income < 3000:
-            print("الدخل الشهري غير كاف")
+    age = check_numeric_value(age,"يرجى ادخال العمر بشكل صحيح: ")
+    monthly_income = input("ادخل قيمة الدخل الشهري: ")
+    monthly_income = check_numeric_value(monthly_income, "يرجى ادخال قيمة الدخل الشهري بشكل صحيح: ")
+    loan_value = input("أدخل قيمة القرض المطلوب: ")
+    loan_value = check_numeric_value(loan_value, "يرجى ادخال قيمة القرض المطلوب بشكل صحيح: ")
+    if age <21 or age >=65:
+        ban_reason.append("العمر غير مناسب")
+    if monthly_income < 3000:
+        ban_reason.append("الدخل الشهري غير كاف")
+    if loan_value <= 5000:
+        ban_reason.append("قيمة القرض أقل من المسموح")
+    if len(ban_reason) == 0:# سوف يتم التحقق من طول القائمة  ban_reason لأنه اذا كان طول هذه القائمة لايساوي الصفر فهذا يعني وجود سبب أولي يمنع البنك من اعطاء القرض للعميل
+        monthly_payment=loan_value * 0.1#حساب قيمة القسط الشهري للقرض
+        if loan_value < 20000:# التحقق من قيمة القرض لمعرفة قيمة التأمين التي سوف يتم فرضها على الزبون
+            insurance=loan_value * 0.015
         else:
-            loan_value=input("أدخل قيمة القرض المطلوب: ")
-            loan_value=check_numeric_value(loan_value,"يرجى ادخال قيمة القرض المطلوب بشكل صحيح")
-            if loan_value <= 5000:
-                print("ٌقيمة القرض أقل من المسموح")
-            else:
-                monthly_payment=loan_value * 0.1
-                if loan_value < 20000:
-                    insurance=loan_value * 0.015
-                else:
-                    insurance=loan_value * 0.01
-                total_monthly_payment=monthly_payment + insurance
-                if total_monthly_payment > monthly_income * 0.3:
-                    print("القسط الشهري يتجاوز 30% من الدخل الشهري")
-                else:
-                    print("أنت مؤهل للحصول على قرض")
-                    print("قيمة القسط الشهري: ",int(monthly_payment))
-                    print("قيمة قسط التأمين: ", int(insurance))
-                    print("المجموع الكلي للقسط الشهري" , int(total_monthly_payment))
+            insurance=loan_value * 0.01
+        total_monthly_payment=monthly_payment + insurance# حساب قيمة القسك الشهري الكامل التي يجب على الزبون دقعها كل شهر
+        if total_monthly_payment > monthly_income * 0.3:#التحقق من أن قيمة القسط الشهري الشامل لا يتجاوز 30% من الدخل الشهري للزبون
+            print("القسط الشهري يتجاوز 30% من الدخل الشهري")
+        else:
+            print("أنت مؤهل للحصول على قرض")
+            print("قيمة القسط الشهري: ",int(monthly_payment))
+            print("قيمة قسط التأمين: ", int(insurance))
+            print("المجموع الكلي للقسط الشهري" , int(total_monthly_payment))
     else:
-        print("العمر غير مناسب")
+        [print(x) for x in ban_reason]
 getting_loan()
 
 
--- subject two.py
+++ subject two.py
@@ -1,32 +1,33 @@
 num_list,pair_list,target_number=[],[],0
 def check_numeric_value(value,note):
     #تعمل هذه الدالة على التأكد من أن القيمة المدخلة رقمية وموجبة
-    if value.isdigit():
-        if int(value) >0:
+    if value.isdigit():# التحقق من أن القيمة المدخلة رقمية
+        if int(value) >=0:# التحقق من أن القيمة المدخلة موجبة
             return int(value)
     else:
         value = input(note)
         while True:
             if value.isdigit():
-                if int(value) > 0:
+                if int(value) >=0:
                     return int(value)
             value = input(note)
 
 def construct_pairs():
-    for z in range(10):
+    for z in range(10):#ادخال الأرقام من قبل المستخدم
         value=input("أدخل الرقم "+str(z+1)+ ": ")
-        global pair_list
-        num_list.append(check_numeric_value(value,"يجب ادخال عدد صحيح موجب"))
-    target_number=input("أدخل الرقم الهدف")
+        global pair_list    # تم تحويل هذا المتحول الى global لتكون القيم التي سوف يأخذها داخل مجال حلقة for متاحة للتعامل معها خارج هذا المجال
+        num_list.append(check_numeric_value(value,"يجب ادخال عدد صحيح موجب")) # التحقق من أن الرقم المدخل رقم صحيح وموجب
+    target_number=input("أدخل الرقم الهدف: ") # ادخال العدد الهدف
     target_number=check_numeric_value(target_number,"يرجى ادخال الرقم الهدف, يجب أن يكون عدداً صحيحاً وموجباً")
-    for x in range(len(num_list)-1):
+    # تعمل حلقات For التالية على البحث عن الأزواج في الأرقام المدخلة التي يمكن أن يكون مجموعها يساوي الرقم الهدف
+    for x in range(len(num_list)-1):#تكرر هذه الحلقة 9 مرات لأنه سوف يتم فحص العدد قبل الأخير مع العدد الأخير في الحلقة التالية
         for y in range(len(num_list)):
-            if num_list[x]+num_list[y] == target_number:
-                pair_list.append(sorted([num_list[x],num_list[y]]))
-    if len(pair_list) == 0:
+            if num_list[x]+num_list[y] == target_number:#التحقق اذا كان مجموع العددين يساوي العدد الهدف
+                pair_list.append(sorted([num_list[x],num_list[y]]))# في حال كان العددين يحققان الشرط يتم انشاء قائمة تحوي العددين بعد ترتيبهما ثم اضافة هذه القائمة الى قائمة pair list
+    if len(pair_list) == 0:#
         print("لا يوجد أي زوج يحقق الهدف")
     else:
-        pair_list=list(set(tuple(x) for x in pair_list))
+        pair_list=list(set(tuple(x) for x in pair_list))#الهدف من هذه السطر التخلص من الأزواج المكررة فالنوع set لا يقبل أن يحوي متغيرات مكررة لذلك في حال وجود tuples  تحوي نفس الأرقام   يتم حذفها
         print(pair_list)
 
 construct_pairs()
